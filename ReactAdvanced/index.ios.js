05c8fd732f3833a697a8e42ffd723950f358b5c1
baoxu <baoxu1223@163.com>
2019-01-25 18:12:04 +0800

    导航跳转

--- ReactAdvanced/index.ios.js
+++ ReactAdvanced/index.ios.js
@@ -5,29 +5,85 @@
  */
 
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Platform
+  Platform,
+  Button,
+  NavigatorIOS
 } from 'react-native';
 
 export default class ReactAdvanced extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit index.ios.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     Press Cmd+R to reload,{'\n'}
+      //     Cmd+D or shake for dev menu
+      //   </Text>
+      // </View>
+
+/* 1、导航跳转*/
+    /*  
+     其中要渲染的组件在路由对象的component字段中指定，
+     要给目标组件传递的参数则写在passProps中。
+     被渲染的 component 都会自动接受到一个名为navigator的属性，
+     你可以直接调用此对象(this.props.navigator)的push和pop方法。
+    */
+
+      <NavigatorIOS
+        initialRoute={{
+          component: MyScene,
+          title: "My Initial Scene",
+          passProps: { index: 1 }
+        }}
+        style={{ flex: 1 }} />
+
+    );
+  }
+}
+
+
+class MyScene extends Component {
+  static propTypes = {
+    route: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    navigator: PropTypes.object.isRequired
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+  }
+
+  _onForward() {
+    let nextIndex = ++this.props.index;
+    this.props.navigator.push({
+      component: MyScene,
+      title: "Scene " + nextIndex,
+      passProps: { index: nextIndex }
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Current Scene: {this.props.title}</Text>
+        <Button
+          onPress={this._onForward}
+          title="Tap me to load the next scene"
+        />
       </View>
     );
   }
